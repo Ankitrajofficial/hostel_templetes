1a450dea7bc1a5eafc7cd03345809dac202f9ac6
Ankit gupta <ankit_gupta@Ankits-MacBook-Air.local>
2025-12-15 19:31:23 +0530

    Refactor branding and site config

--- js/site-config.js
+++ js/site-config.js
@@ -1,19 +1,19 @@
 const SiteConfig = {
   // Brand Identity
-  brandName: "MK HEIGHT",
-  brandNameMeta: "MK HEIGHT | Premium Luxury Hostel", // For titles
-  brandLogoText: "MK HEIGHT",
+  brandName: "KANCHAN MOHAN",
+  brandNameMeta: "KANCHAN MOHAN | Premium Luxury Girls Hostel", // For titles
+  brandLogoText: "KANCHAN MOHAN",
 
   // Contact Information
   address:
     "MK Heights, Coral Park, Near Suwalka Emerald, Bundi Road, Kunadi, Kota, Rajasthan 324008",
   shortAddress: "Kunadi, Kota, Rajasthan",
-  phone: "+91 9116666999", // Replace if needed, taking from footer/contact
-  email: "info@mkheight.com", // Placeholder, didn't see explicit email in grep, using generic
+  phone: "+91 7073478099", // Replace if needed, taking from footer/contact
+  email: "info@kanchanmohan.com", // Placeholder, didn't see explicit email in grep, using generic
 
   // External Links
   googleMapsLink:
-    "https://www.google.com/maps/search/?api=1&query=MK+Heights+Kota", // Simple search or specific link
+    "https://www.google.co.in/maps/place/Kanchan+Mohan+girls+hostel/@25.2020207,75.8283295,17z/data=!4m10!1m2!2m1!1skanchan+mohan+girls+hostel+landmark+google+map+link%C2%A0!3m6!1s0x396f9b7f26f3ed87:0x1b78e04adc0c4ddd!8m2!3d25.2022158!4d75.8315583!15sCjVrYW5jaGFuIG1vaGFuIGdpcmxzIGhvc3RlbCBsYW5kbWFyayBnb29nbGUgbWFwIGxpbmvCoCIDiAEBWjUiM2thbmNoYW4gbW9oYW4gZ2lybHMgaG9zdGVsIGxhbmRtYXJrIGdvb2dsZSBtYXAgbGlua5IBDGdpcmxzX2hvc3RlbJoBI0NoWkRTVWhOTUc5blMwVkpRMEZuU1VOMWVHOTFOMlpuRUFF4AEA-gEECB4QHA!16s%2Fg%2F11g0mf6dg9?entry=ttu&g_ep=EgoyMDI1MTIwOS4wIKXMDSoASAFQAw%3D%3D", // Simple search or specific link
 
   // Social Media (Examples)
   instagram: "#",

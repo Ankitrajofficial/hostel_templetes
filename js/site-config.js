afcc615056c42361860b63f90792e2116af91a39
Ankit raj <businesswithankit.0@gmail.com>
2025-12-19 15:40:25 +0530

    Update site configuration for brand and contact info

--- js/site-config.js
+++ js/site-config.js
@@ -1,19 +1,19 @@
 const SiteConfig = {
   // Brand Identity
-  brandName: "KANCHAN MOHAN",
-  brandNameMeta: "KANCHAN MOHAN | Premium Luxury Girls Hostel", // For titles
-  brandLogoText: "KANCHAN MOHAN",
+  brandName: "VISHALAKSHI RESIDENCY",
+  brandNameMeta: "VISHALAKSHI RESIDENCY | Premium Luxury Girls Hostel", // For titles
+  brandLogoText: "VISHALAKSHI RESIDENCY",
 
   // Contact Information
   address:
-    "MK Heights, Coral Park, Near Suwalka Emerald, Bundi Road, Kunadi, Kota, Rajasthan 324008",
+    "I 13-14, Premium Girls Hostel in Landmark City, &, Samyak Rd, near Allen Sangyan, Kunadi, Electricity Board Area, Kota, Rajasthan 324008",
   shortAddress: "Kunadi, Kota, Rajasthan",
-  phone: "+91 7073478099", // Replace if needed, taking from footer/contact
-  email: "info@kanchanmohan.com", // Placeholder, didn't see explicit email in grep, using generic
+  phone: "+91-9352600581", // Replace if needed, taking from footer/contact
+  email: "vishalakshiresidency@gmail.com", // Placeholder, didn't see explicit email in grep, using generic
 
   // External Links
   googleMapsLink:
-    "https://www.google.co.in/maps/place/Kanchan+Mohan+girls+hostel/@25.2020207,75.8283295,17z/data=!4m10!1m2!2m1!1skanchan+mohan+girls+hostel+landmark+google+map+link%C2%A0!3m6!1s0x396f9b7f26f3ed87:0x1b78e04adc0c4ddd!8m2!3d25.2022158!4d75.8315583!15sCjVrYW5jaGFuIG1vaGFuIGdpcmxzIGhvc3RlbCBsYW5kbWFyayBnb29nbGUgbWFwIGxpbmvCoCIDiAEBWjUiM2thbmNoYW4gbW9oYW4gZ2lybHMgaG9zdGVsIGxhbmRtYXJrIGdvb2dsZSBtYXAgbGlua5IBDGdpcmxzX2hvc3RlbJoBI0NoWkRTVWhOTUc5blMwVkpRMEZuU1VOMWVHOTFOMlpuRUFF4AEA-gEECB4QHA!16s%2Fg%2F11g0mf6dg9?entry=ttu&g_ep=EgoyMDI1MTIwOS4wIKXMDSoASAFQAw%3D%3D", // Simple search or specific link
+    "https://www.google.com/maps/place/VISHALAKSHI+RESIDENCY%7C+Best+Girls+Hostel+in+Kunadi+Kota+%7C+Girls+PG+in+Kunadi+%7C+Girls+PG+in+kota/@25.198184,75.827094,16978m/data=!3m1!1e3!4m6!3m5!1s0x396f9bfddd9c02bd:0x187f193dfbf58a49!8m2!3d25.1981839!4d75.8270942!16s%2Fg%2F11h6crdqw6?hl=en&entry=ttu&g_ep=EgoyMDI1MTIwOS4wIKXMDSoASAFQAw%3D%3D", // Simple search or specific link
 
   // Social Media (Examples)
   instagram: "#",
